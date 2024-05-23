0cbcf67515f0fd494e359581cf9bd268b372cd73
karan8404 <kkawathia8404@gmail.com>
2024-05-23 20:07:49 +0530

    added https

--- components/socket.ts
+++ components/socket.ts
@@ -4,5 +4,5 @@ config({ path: './.env',debug:true});
 import { io } from "socket.io-client";
 const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
 const port = process.env.PORT || 3000;
-const socketURL =`http://${host}:${port}`;
+const socketURL =`https://${host}:${port}`;
 export const socket = io(socketURL);
