82b0fef69763d92c1ef8a01c932338dce7bc3d41
karan8404 <kkawathia8404@gmail.com>
2024-05-24 20:08:35 +0530

    refactor: Update getUser route to use Response instead of NextResponse

--- app/api/getUser/route.ts
+++ app/api/getUser/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest} from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { options } from "../auth/[...nextauth]/options";
 
@@ -7,17 +7,17 @@ export const prisma = new PrismaClient();
 const getUser = async (req: NextRequest) => {
   const email = req.nextUrl.searchParams.get("email");
   if (!email) {
-    return NextResponse.json({ error: "Email is required" }, { status: 400 });
+    return Response.json({ error: "Email is required" }, { status: 400 });
   }
   const user = await prisma.user.findUnique({
     where: { email },
   });
 
   if (!user) {
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
+    return Response.json({ error: "User not found" }, { status: 404 });
   }
 
-  return NextResponse.json(
+  return Response.json(
     { userID: user?.id, name: user?.name },
     { status: 200 }
   );
--- socket.ts
+++ socket.ts
@@ -1,11 +1,3 @@
-import { config } from "dotenv";
-config({ path:'.env',debug: true });
 import { io } from "socket.io-client";
 
-const port = parseInt(process.env.PORT);
-console.log(port);
-const socketURL =
-  port === 3000
-    ? "http://localhost:3000" 
-    : `https://minigames.railway.internal:${port}`;
 export const socket = io();
