bd3a210e94c9f0620ac946daf04e824e3f94f3f0
karan8404 <kkawathia8404@gmail.com>
2024-05-24 14:33:32 +0530

    .

--- socket.ts
+++ socket.ts
@@ -7,5 +7,5 @@ console.log(port);
 const socketURL =
   port === 3000
     ? "http://localhost:3000"
-    : `http://minigames.railway.internal:${port}`;
+    : `https://minigames.railway.internal:${port}`;
 export const socket = io(socketURL);
