a9477dc83bacd120d49b0803bd5223b0f41bb06d
karan8404 <kkawathia8404@gmail.com>
2024-05-25 19:15:18 +0530

    Updated next Auth to use callback to handle prisma user creation

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,24 +1,45 @@
-import type { NextAuthOptions } from "next-auth"
+import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const options: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string,
-            allowDangerousEmailAccountLinking: true
-        }),
-        GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-            allowDangerousEmailAccountLinking: true
-        })
-    ],
-    
-}
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (account.provider === "google" || account.provider === "github") {
+        const prismaUser = await prisma.user.findFirst({
+          where: {
+            email: user.email,
+          },
+        });
+        if (!prismaUser) {
+          console.log("creating user", user);
+          await prisma.user.create({
+            data: {
+              email: user.email,
+              name: user.name,
+              image: user.image,
+            },
+          });
+        } else {
+          console.log("user already exists", user);
+        }
+      }
+      return true;
+    },
+  },
+};
--- app/api/getUser/route.ts
+++ app/api/getUser/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest} from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { options } from "../auth/[...nextauth]/options";
 
 export const prisma = new PrismaClient();
 
