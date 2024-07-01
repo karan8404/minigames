5026f04b8aa5b76c91051859d8c905bbdf7b7ef7
karan8404 <kkawathia8404@gmail.com>
2024-07-01 19:50:34 +0530

    changed img to Image

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { Card, CardContent, CardHeader, CardTitle, } from "@/components/ui/card"
+import Image from 'next/image'
 
 
 export default function GameCard(props: Prop) {
@@ -12,7 +13,7 @@ export default function GameCard(props: Prop) {
               <CardTitle className='text-center'>{props.title}</CardTitle>
             </CardHeader>
             <CardContent>
-              <img src={props.img} alt="" className='flex items-center justify-center h-56 w-56 object-scale-down' />
+              <Image src={props.img} alt="" className='flex items-center justify-center h-56 w-56 object-scale-down' />
             </CardContent>
           </Card>
         </Link>
